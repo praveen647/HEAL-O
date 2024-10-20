81fd2c73fb50c675a50350b673bfd9ff2370e84a
Praveen Kumar R <133590552+praveen647@users.noreply.github.com>
2024-10-20 08:29:02 +0530

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://disease-prediction-u62r.onrender.com/',
+  baseURL: 'https://disease-prediction-u62r.onrender.com',
   headers: {
-    'ngrok-skip-browser-warning': 'true',
     'Content-Type': 'application/json'
 }
 });
